5693dd2aa9fb59d938ca1be52830cc3123623b47
Sererim <danielleymanw@gmail.com>
2023-10-10 19:55:43 +0300

    Final commit. No errors.

--- django_third/testapp/views.py
+++ django_third/testapp/views.py
@@ -1,3 +1,31 @@
 from django.shortcuts import render
+from django.utils.timezone import datetime, timedelta
+from .models import Client, Order
 
-# Create your views here.
+
+def order_list(request):
+    if request.method == 'POST':
+        firstname = request.POST.get('firstname')
+        surname = request.POST.get('surname')
+        timeframe = request.POST.get('timeframe')
+
+        if firstname and surname and timeframe:
+            if timeframe == '7':
+                start_date = datetime.now() - timedelta(days=7)
+            elif timeframe == '30':
+                start_date = datetime.now() - timedelta(days=30)
+            elif timeframe == '365':
+                start_date = datetime.now() - timedelta(days=365)
+            else:
+                pass
+
+            client = Client.objects.filter(firstname=firstname, surname=surname).first()
+            orders = Order.objects.filter(client_id=client, date_of_order__gte=start_date)
+            
+            context = {
+                'orders': orders,
+                'client': client,
+            }
+            return render(request, 'testapp/order_list.html', context)
+
+    return render(request, 'testapp/index.html')
